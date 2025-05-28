d1d2f7822e7a284976eed7bde3ca697f343ea72c
LorenzoPiatti <lorenzopiatti2003@gmail.com>
2025-05-28 15:46:23 -0300

    .

--- Trivia/js/login.js
+++ Trivia/js/login.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
             formulario.reset();
             resetearValidacion();
             alert("¡Inicio de sesión exitoso!");
-            window.location.href = "trivia.html"
+            window.location.href = "main.html"
         }
     });
 });
