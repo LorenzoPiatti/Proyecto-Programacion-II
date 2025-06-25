357ecfd861806e3eaac3fe065aa753277626aa43
LorenzoPiatti <lorenzopiatti2003@gmail.com>
2025-06-25 15:11:04 -0300

    a ver ahora...

--- Trivia/static/js/main.js
+++ Trivia/static/js/main.js
@@ -4,7 +4,4 @@ window.addEventListener("DOMContentLoaded", () => {
         const saludo = document.getElementById("saludoUsuario");
         saludo.textContent = `Hola, ${nombre}`;
     }
-    const audio = new Audio("static/audio/lobby.mp3");
-    audio.loop = true;
-    audio.play();
 });
